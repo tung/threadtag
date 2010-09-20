00e28ff8aaf82d566b8fd1057b0f20b5b56e9219
Tung Nguyen <tunginobi@gmail.com>
2010-09-20 23:05:03 +1000

    Create database from schema.

--- Gemfile
+++ Gemfile
@@ -1,3 +1,9 @@
 source "http://rubygems.org"
 
 gem "sinatra"
+gem "sequel"
+gem "sinatra-sequel"
+
+group :development do
+  gem "sqlite3-ruby"
+end
--- app.rb
+++ app.rb
@@ -1,6 +1,32 @@
 require "rubygems"
 require "bundler/setup"
+
 require "sinatra"
+require "sinatra/sequel"
+
+# Connect to database.
+set :database, ENV['DATABASE_URL'] || 'sqlite://threadtag.db'
+
+
+### MIGRATIONS
+
+migration "create threadtag table" do
+  database.create_table :threadtag do
+    String   :board, :size => 16
+    Integer  :thread
+    String   :tag, :size => 32
+    String   :ip, :size => 16
+    DateTime :updated_at
+
+    primary_key [:board, :thread, :tag, :ip]
+    index :board
+    index [:board, :thread]
+    index [:board, :tag]
+  end
+end
+
+
+### APP
 
 get '/hi' do
   "Hello world!"
